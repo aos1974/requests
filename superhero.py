e63d6eca4fb47a6f8f541fdd3694046637265080
Aleksandr Sysoev <aosysoev@yandex.ru>
2022-03-23 23:04:46 +0300

    Task №1 - compleated & commited

--- superhero.py
+++ superhero.py
@@ -1,10 +1,13 @@
 import requests
+import json
 from tokenname import get_connetion_str
 
 #
 # Определение глобальных переменных и констант
 #
 
+# коды HTTP ошибок и статусов запросов
+HTTP_OK = 200
 # файл содержащий токен для доступа к api
 TOKEN_FILE = 'superhero.token'
 # адрес сервиса
@@ -17,26 +20,42 @@ SUPERHEROES = ['Hulk', 'Captain America', 'Thanos']
 #
 
 # функция получения информации о супергерое
-def get_superhero_stats(url: str) -> dict:
+def get_superhero_stats(superhero: str) -> int:
 
+    intelligence = -1
+    # формируем строку подключения к сервису
+    url = get_connetion_str(TOKEN_FILE, URL) + '/search/' + superhero
     # запрашиваем информацию о супергерое
     response = requests.get(url)
+    # определяем уровень интелекта супергероя
+    if response.status_code == HTTP_OK:
+        js = json.loads(response.text)
+        intelligence = js['results'][0]['powerstats']['intelligence']
 
-    return response
+    return intelligence
 #
 # Главная функция программы
 #
 
-def main() -> bool:
+def main() -> str:
 
+    # код завершения работы программы
+    error_code = 'ERROR: работа программы завершена с ошибкой'
+    superhero_dict = {}
+    # запрашиваем уровень интелекта супергероя
     for superhero in SUPERHEROES:
-        x = get_superhero_stats(get_connetion_str(TOKEN_FILE, URL) + '/' + superhero)
-        print(x)
+        superhero_level = get_superhero_stats(superhero)
+        if superhero_level == -1:
+            return error_code
+        else:
+            superhero_dict[int(superhero_level)] = superhero
+    # определяем самого умного супергероя
+    error_code = superhero_dict.get(max(superhero_dict.keys()))
 
-    return True
+    return 'Самый умный супергерой это - ' + error_code
 #
 # Основная программа
 #
 
 if __name__ == '__main__':
-    main()
+    print(main())
